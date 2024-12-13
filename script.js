63fb8cde439f2e8b694b4afca8b50317943eecdc
UniversalDummy <davyhawk0109@gmail.com>
2024-12-13 14:41:06 -0500

    Unit 1 Lesson 2 is almost complete
    This is kind of cool. It adds interactive features.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let x = document.cookie = ""
+
+
+if (document.cookie == undefined) {
+  console.log("true")
+  document.cookie = `lessonsComplete=; expires Tue, 31 Dec 2024 12:00:00 UTC path=/Demo.html`
+}
+
 
 let coutIntro = document.getElementById("IntroToCout");
 
@@ -6,3 +14,67 @@ function OpenToLessons(type, number) {
     window.location.href = url;
 
 }
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let completeLessons = getCookie("lessonsComplete");
+
+    let highlight = [];
+
+    let lesson = "";
+    for (let i = 0; i < completeLessons.length; i++)  {
+        if (completeLessons[i] != ",") {
+          lesson += completeLessons[i]
+          console.log(lesson)
+        }
+        else {
+          highlight.push(lesson);
+          lesson = ""
+        }
+    }
+    console.log(highlight);
+
+    for (let i = 0; i < highlight.length; i++) {
+      document.getElementById(highlight[i]).classList.add("bg-success", "text-light")
+    }
+    
+    
+
+}
+
+function clear_cooks() {
+  var x;
+  if (confirm("Are you sure you would like to reset your lessons?") == true) {
+      x = true;
+  } else {
+      x = false;
+  }
+
+  if (x) {
+    document.cookie = "lessonsComplete=;";
+    location.reload(true);
+  }
+  
+}
+
+function Show_cooks() {
+  alert(document.cookie)
+}
+
+
+checkCookie();
--- scripts/LessonPage.js
+++ scripts/LessonPage.js
@@ -6,11 +6,12 @@ let LessonNumber = queryParams.get('LessonNumber');
 let NumberSel
 let TypeSel
 let LessonTitle = document.getElementById("LessonTitle")
+let TabTitle = document.getElementById("title");
 
 
 
 switch(LessonType) {
-    case "cout":
+    case "A":
         console.log("Lesson set to COUT")
         TypeSel = document.getElementById("CoutCarousel")
 
@@ -25,6 +26,7 @@ switch(LessonType) {
                 console.log("COUT: 1");
 
                 NumberSel = document.getElementById("1");
+                TabTitle.innerText += "Introduction to Cout";
 
                 NumberSel.classList.add("active")
 
@@ -36,6 +38,8 @@ switch(LessonType) {
                 console.log("COUT: 2");
 
                 NumberSel = document.getElementById("2");
+
+                TabTitle.innerText += "Using Cout"
 
                 NumberSel.classList.add("active")
 
@@ -60,3 +64,100 @@ switch(LessonType) {
         break
 }
 
+
+
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+
+function cookLesson(lesson) {
+    
+    let completeLessons = getCookie("lessonsComplete");
+
+    completeLessons += lesson += ",";
+
+    x = document.cookie = `lessonsComplete=${completeLessons}; expires Sun, 15 Dec 2024 12:00:00 UTC path=/Demo.html`;
+
+}
+
+function check(answer, problem) {
+  let answerBox = document.getElementById(answer);
+
+  console.log(answerBox)
+
+  switch (answer) {
+    case "inputField0":
+
+      let htag = document.getElementById("result_h")
+      let ptag = document.getElementById("result_p")
+
+
+      if (CutSpaces(answerBox.value) == "std::cout<<\"Hello World!\";") {
+        
+        htag.classList.remove("text-danger")
+        htag.classList.add("text-success")
+        htag.innerText = "Great Job!"
+        
+
+        ptag.innerText = 'You included the std:: namespace, along with the cout object and insertion operator.\nYour text that you chose to output was "Hello World!"\nKeep up the good work!'
+      }
+      else {
+        htag.classList.remove("text-success")
+        htag.classList.add("text-danger")
+        htag.innerText = "Uh Oh..."
+
+        ptag.innerText = 'Did you include the correct namespace?\nAre you using the cout object in your line of code?\nDid you use the insertion operator?\nAre you sure that you are trying to print "Hello World!"?\nCould you have forgotten your semicolon?'
+      }
+      break;
+  }
+  
+}
+
+function CutSpaces(string) {
+  let outcome = "";
+  let isntQuote = true
+
+  console.log(string.length)
+
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] == "\"") {
+      if (isntQuote) {
+        isntQuote = false
+      }
+      else {
+        isntQuote = true
+      }
+    }
+    
+    if (isntQuote) {
+      if (string[i] != " ") {
+        outcome += string[i];
+      }
+    }
+    else {
+      outcome += string[i]
+    }
+  }
+
+  console.log(outcome);
+  return outcome;
+
+}
+
+
+
+console.log(CutSpaces("T H I S _ I S _ S E P A R A T E D _ B Y _ S P A C E S, \"A N D T H I S I S N T\""));
